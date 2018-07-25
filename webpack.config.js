e0f6b0776bfd42f1053b4e7398d13576f29251d9
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-07-25 21:20:55 +0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    devServer: {
+        host: '127.0.0.1',
+        port: 3000,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            autoprefixer: {
+                                browsers: ['last 2 versions']
+                            },
+                            plugins: () => [autoprefixer]
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {}
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8000,
+                            name: 'assets/[name]-[hash].[ext]'
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            optipng: {
+                                enabled: true
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false
+                            },
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Brenda',
+            hash: true,
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name]-styles.css',
+            chunkFilename: '[id].css'
+        }),
+        new CleanWebpackPlugin('dist', {})
+    ]
+};
