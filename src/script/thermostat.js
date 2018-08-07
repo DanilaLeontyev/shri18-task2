318801669f5f3ee4e71207a0d4184aca87c6c46f
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-08-07 21:28:27 +0500

    убрал картинку термостата, сделал с помощью теней

--- src/script/thermostat.js
+++ src/script/thermostat.js
@@ -1,15 +1,8 @@
 import { Draggable } from 'gsap/draggable'
 
-const rect = document.querySelector('#controller')
-
-rect.addEventListener('mousedown', function (event) {
-    console.log('sdfj')
-})
-
 Draggable.create('#controller', {
     type: "rotation",
     throwProps: true,
-    trigger: "#knob",
     bounds: { maxRotation: 135, minRotation: -135 },
     onClick: function () {
         console.log("clicked");
