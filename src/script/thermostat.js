1f3481f55652bb110084aa52e54c34175c86dd0b
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-08-07 20:37:54 +0500

    добавил библиотеку draggable

---
+++ src/script/thermostat.js
@@ -0,0 +1,20 @@
+import { Draggable } from '@shopify/draggable'
+
+const rect = document.querySelector('#controller')
+
+rect.addEventListener('mousedown', function (event) {
+    console.log('sdfj')
+})
+
+Draggable.create('#controller', {
+    type:"rotation",
+	throwProps: true,
+	trigger:"#knob",
+	bounds:{maxRotation:135, minRotation:-135},
+	onClick:function() {
+		console.log("clicked");
+	},
+	onThrowComplete:function() {
+		console.log(this.rotation);		
+	}
+})
