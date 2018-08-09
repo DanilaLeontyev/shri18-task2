05ac43ef09c65e7e8abac717ec313d5c42657622
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-08-09 21:27:44 +0500

    эксперимент с окантовкой регулятора

--- src/script/thermostat.js
+++ src/script/thermostat.js
@@ -44,6 +44,31 @@ function moveController(event) {
     console.log(degree)
 }
 
+let angle = 0;
+let rotate_x = 120;
+let rotate_y = 0;
+
+function vec2ang(x, y) {
+    let angleInRadians = Math.atan2(y, x);
+    let angleInDegrees = (angleInRadians / Math.PI) * 180;
+    return angleInDegrees;
+}
+
+let notchContainer = document.querySelector('.notch-container')
+
+for (let i = 0; i < 60; i++) {
+    let thin = document.createElement("div");
+    let x = rotate_x * Math.cos(angle) - rotate_y * Math.cos(angle);
+    let y = rotate_y * Math.cos(angle) + rotate_x * Math.sin(angle);
+    let r = vec2ang(x, y);
+    thin.className = "thin";
+    thin.style.left = 127 + x + "px";
+    thin.style.top = 127 + y + "px";
+    thin.style.transform = "rotate(" + r + "deg)";
+    notchContainer.appendChild(thin);
+    angle += (Math.PI / 300) * 10;
+}
+
 function updateValue(degree) {
 
 }
