3e121753d289a483f1e8f28c60621f708ff5f9bc
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-08-12 19:11:15 +0500

    подкорректировал стили и логику движения ползунков

--- src/script/sliderThermostat.js
+++ src/script/sliderThermostat.js
@@ -1,32 +1,39 @@
 let app = (() => {
+  function updateSlider(element) {
+    if (element) {
+      let parent = element.parentElement,
+        lastValue = parent.getAttribute('data-slider-value');
 
-    function updateSlider(element) {
-        if (element) {
-            let parent = element.parentElement,
-                lastValue = parent.getAttribute('data-slider-value');
+      if (lastValue === element.value) {
+        return; // No value change, no need to update then
+      }
 
-            if (lastValue === element.value) {
-                return; // No value change, no need to update then
-            }
-
-            parent.setAttribute('data-slider-value', element.value);
-            let $thumb = parent.querySelector('.range-slider__thumb'),
-                pct = element.value * ((parent.clientHeight - $thumb.clientHeight) / parent.clientHeight);
-
-            $thumb.style.bottom = `${pct}%`;
-        }
+      parent.setAttribute('data-slider-value', element.value);
+      let thumb = parent.querySelector('.range-slider__thumb');
+      let newVal =
+        Math.abs(element.value) *
+        ((parent.clientHeight - thumb.clientHeight) /
+          (element.max - element.min));
+      console.log(newVal);
+      thumb.style.bottom = `${newVal}px`;
     }
-    return {
-        updateSlider: updateSlider
-    };
-
+  }
+  return {
+    updateSlider: updateSlider
+  };
 })();
 
 (function initAndSetupTheSliders() {
-    const inputs = [].slice.call(document.querySelectorAll('.range-slider input'));
-    inputs.forEach(input => input.setAttribute('value', '23'));
-    inputs.forEach(input => app.updateSlider(input));
+  const inputs = [].slice.call(
+    document.querySelectorAll('.range-slider input')
+  );
+  inputs.forEach(input => input.setAttribute('value', '23'));
+  inputs.forEach(input => app.updateSlider(input));
 
-    inputs.forEach(input => input.addEventListener('input', element => app.updateSlider(input)));
-    inputs.forEach(input => input.addEventListener('change', element => app.updateSlider(input)));
+  inputs.forEach(input =>
+    input.addEventListener('input', element => app.updateSlider(input))
+  );
+  inputs.forEach(input =>
+    input.addEventListener('change', element => app.updateSlider(input))
+  );
 })();
