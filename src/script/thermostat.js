52f59b6fa8e67aa3f95f6c413c5c1db6bf9c13f6
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-08-19 14:06:44 +0500

    сделал правильную подсветку круга в термостате

--- src/script/thermostat.js
+++ src/script/thermostat.js
@@ -1,5 +1,7 @@
 const controller = document.querySelector('#controller');
 const temperature = document.querySelector('.temperature');
+let minAngle = 30;
+let maxAngle = 330;
 
 controller.addEventListener('mousedown', function() {
   this.addEventListener('mousemove', moveController);
@@ -16,39 +18,34 @@ controller.addEventListener('touchstart', function() {
 });
 
 drawNotch();
+
 function moveController(event) {
   event.preventDefault();
-  let minAngle = 150;
-  let maxAngle = 205;
+
   let rect = this.getBoundingClientRect();
   let x = rect.left + rect.width / 2;
   let y = rect.top + rect.height / 2;
-
   let mouseX = event.pageX;
   let mouseY = event.pageY;
+  let degree = vec2ang(mouseX - x, mouseY - y);
 
-  let degree = vec2ang(mouseX - x, mouseY - y) + 90;
-  if (degree >= minAngle && degree <= maxAngle) {
+  if (degree <= minAngle || degree >= maxAngle) {
     return;
   }
-
+  console.log(degree);
   drawNotch(degree);
   this.style.transform = `rotate(${degree}deg)`;
 }
 
 function moveControllerTouch(event) {
   event.preventDefault();
-  let minAngle = 150;
-  let maxAngle = 205;
   let rect = this.getBoundingClientRect();
   let x = rect.left + rect.width / 2;
   let y = rect.top + rect.height / 2;
-
   let mouseX = event.changedTouches[0].pageX;
   let mouseY = event.changedTouches[0].pageY;
-
-  let degree = vec2ang(mouseX - x, mouseY - y) + 90;
-  if (degree >= minAngle && degree <= maxAngle) {
+  let degree = vec2ang(mouseX - x, mouseY - y);
+  if (degree <= minAngle || degree >= maxAngle) {
     return;
   }
   drawNotch(degree);
@@ -63,23 +60,21 @@ function drawNotch(degree) {
   let lineCount = 360 / 2.5;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.lineWidth = 1;
-  ctx.strokeStyle = '#808080';
-  ctx.strokeStyle = '#F8C46D';
 
   for (let i = 0; i < lineCount; i++) {
     let t = i * (360 / lineCount);
     let tr = degToRad(t - 90) % 360;
 
-    if (t > 330 || t < 30) continue;
+    if (t <= minAngle || t >= maxAngle) {
+      continue;
+    }
     let fromX = canvas.width / 2 + radius * Math.cos(tr);
     let fromY = canvas.height / 2 + radius * Math.sin(tr);
 
     let toX = canvas.width / 2 + (radius + lineHeight) * Math.cos(tr);
     let toY = canvas.height / 2 + (radius + lineHeight) * Math.sin(tr);
 
-    t - 185 > degree
-      ? (ctx.strokeStyle = '#808080')
-      : (ctx.strokeStyle = '#F8C46D');
+    t > degree ? (ctx.strokeStyle = '#808080') : (ctx.strokeStyle = '#F8C46D');
 
     ctx.beginPath();
     ctx.moveTo(fromX, fromY);
@@ -89,8 +84,11 @@ function drawNotch(degree) {
 }
 
 function vec2ang(x, y) {
-  let angleInRadians = Math.atan2(y, x);
-  let angleInDegrees = (angleInRadians / Math.PI) * 180;
+  let angleInRadians = Math.atan2(y, x) - Math.PI / 2;
+  let angleInDegrees = radToDeg(angleInRadians);
+  if (angleInRadians < 0) {
+    angleInDegrees += 360;
+  }
   return angleInDegrees;
 }
 
